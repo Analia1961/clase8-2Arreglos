d7e86681cf8d3a7af19813b16cbb58084600ce10
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-05-20 15:41:34 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// Arreglos (Ejercicios ONLINE) Viernes 13 de mayo de 2022
+// Ejercicio 5 - Dos Arreglos
+
+/*
+• Construya un algoritmo que tenga dos arreglos uno que almacene 2 nombres y otro que almacene 3 números ambos ingresados por el usuario.
+• Al final debe imprimir los valores por consola.
+*/
